5fd4041aaff33f81aba4eb3a24055b8df27d2b2c
dcucereavii <dcucereavii@microsoft.com>
2024-01-19 15:59:10 -0500

    original commit

---
+++ SimpleBrokeredMessaging.Receiver/ReceiverConsole.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace SimpleBrokeredMessaging.Receiver
+{
+    internal class ReceiverConsole
+    {
+        //ToDo: Enter a valid Service Bus connection string
+        static string ConnectionString = "Endpoint=sb://messaging-demo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=wtCy236mj0ZsseiKh5XBZMINNvce2Ae11+ASbEicU4E=";
+        static string QueueName = "demoqueue";
+
+
+        static async Task Main(string[] args)
+        {
+            // Create a service bus client
+            var client = new ServiceBusClient(ConnectionString);
+
+            // Create a service bus receiver
+            var receiver = client.CreateReceiver(QueueName);
+
+
+            // Receive the messages
+            Console.WriteLine("Receive messages...");
+            while (true)
+            {
+                var message = await receiver.ReceiveMessageAsync();
+
+                if (message != null)
+                {
+                    Console.Write(message.Body.ToString());
+
+                    // Complete the message
+                    await receiver.CompleteMessageAsync(message);
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("All messages received.");
+                    break;
+                }
+            }
+
+            // Close the receiver
+            await receiver.CloseAsync();
+        }
+    }
+}
